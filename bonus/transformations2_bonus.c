aee711a1176f74a7c6304f9b1ed2709a67fef3f2
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2022-11-02 14:01:07 -0300

    Change rotation from 0.0 to middle of img

--- bonus/transformations2_bonus.c
+++ bonus/transformations2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 00:27:36 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/10/30 15:01:30 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/11/02 13:39:40 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,23 @@ void	rotate_x(t_coord *points, double angle, int size)
 {
 	double	cos_theta;
 	double	sin_theta;
-	double	theta;
 	t_coord	holder;
+	t_coord	offset;
 	int		i;
 
-	theta = dg2_rad(angle);
-	cos_theta = cos(theta);
-	sin_theta = sin(theta);
+	cos_theta = cos(dg2_rad(angle));
+	sin_theta = sin(dg2_rad(angle));
 	i = 0;
+	offset.y = points[size / 2].y;
+	offset.z = points[size / 2].z;
 	while (i < size)
 	{
-		holder.x = points[i].x;
-		holder.y = points[i].y;
-		holder.z = points[i].z;
+		holder.y = points[i].y - offset.y;
+		holder.z = points[i].z - offset.z;
 		points[i].y = (holder.y * cos_theta) - (holder.z * sin_theta);
 		points[i].z = (holder.y * sin_theta) + (holder.z * cos_theta);
+		points[i].y = points[i].y + offset.y;
+		points[i].z = points[i].z + offset.z;
 		i++;
 	}
 }
@@ -40,21 +42,23 @@ void	rotate_y(t_coord *points, double angle, int size)
 {
 	double	cos_theta;
 	double	sin_theta;
-	double	theta;
 	t_coord	holder;
+	t_coord offset;
 	int		i;
 
-	theta = dg2_rad(angle);
-	cos_theta = cos(theta);
-	sin_theta = sin(theta);
+	cos_theta = cos(dg2_rad(angle));
+	sin_theta = sin(dg2_rad(angle));
+	offset.x = points[size / 2].x;
+	offset.z = points[size / 2].z;
 	i = 0;
 	while (i < size)
 	{
-		holder.x = points[i].x;
-		holder.y = points[i].y;
-		holder.z = points[i].z;
+		holder.x = points[i].x - offset.x;
+		holder.z = points[i].z - offset.z;
 		points[i].x = (holder.x * cos_theta) + (holder.z * sin_theta);
 		points[i].z = (holder.x * (sin_theta * -1)) + (holder.z * cos_theta);
+		holder.x = points[i].x + offset.x;
+		holder.z = points[i].z + offset.z;
 		i++;
 	}
 }
@@ -63,21 +67,23 @@ void	rotate_z(t_coord *points, double angle, int size)
 {
 	double	cos_theta;
 	double	sin_theta;
-	double	theta;
 	t_coord	holder;
+	t_coord offset;
 	int		i;
 
-	theta = dg2_rad(angle);
-	cos_theta = cos(theta);
-	sin_theta = sin(theta);
+	cos_theta = cos(dg2_rad(angle));
+	sin_theta = sin(dg2_rad(angle));
+	offset.x = points[size / 2].x;
+	offset.y = points[size / 2].y;
 	i = 0;
 	while (i < size)
 	{
-		holder.x = points[i].x;
-		holder.y = points[i].y;
-		holder.z = points[i].z;
+		holder.x = points[i].x - offset.x;
+		holder.y = points[i].y - offset.y;
 		points[i].x = (holder.x * cos_theta) - (holder.y * sin_theta);
 		points[i].y = (holder.x * sin_theta) + (holder.y * cos_theta);
+		holder.x = points[i].x + offset.x;
+		holder.y = points[i].y + offset.y;
 		i++;
 	}
 }
